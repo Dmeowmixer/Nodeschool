cce8d1a90bada81d7af82d86f6612666265fc917
dmeowmixer <dmeowmixer@hotmail.com>
2015-02-24 21:14:55 -1000

    did some stuff

--- functionaljs.js
+++ functionaljs.js
@@ -1,3 +1,4 @@
+
 // function helloworld(string) {
 //   return string.toUpperCase();
 // }
@@ -26,11 +27,87 @@
 //   return messages.filter(isShort).map(toArr);
 // }
 
-function checkUsersValid(goodUsers){
-  return function(submittedUsers){
+// function checkUsersValid(goodUsers){
+//   return function(submittedUsers){
     
-  }
+//   };
+// }
+
+
+// module.exports = checkUsersValid;
+
+
+
+
+// function higherOrder (sumfun, num){
+//   for (var i = 0; i < num; i++){
+//     sumfun(num[i]);
+//   }
+// }
+// module.exports = higherOrder;
+
+
+
+    // function doubleAll(numbers) {
+    //   var result = [];
+    //   for (var i = 0; i < numbers.length; i++) {
+    //     result.push(numbers[i] * 2);
+    //   }
+    //   return result;
+    // }
+    
+//  i need to push each index *2 of numbers into result 
+//     function doubleAll(numbers){
+//       var result = [];
+//       var doubled = numbers.map(function(num){
+//         return num*2;
+//       });
+//       return doubled;
+//     }
+// module.exports = doubleAll;
+
+
+// function getShortMessages(messages){
+//   var mapped = messages.map( function (obj){
+//         return obj.message;
+//   });
+//   return mapped.filter(function(str){
+//     return str.length <= 50;
+//   });
+// }
+// module.exports = getShortMessages;
+
+
+
+
+
+function checkUsersValid(goodUsers) {
+  return function(submittedUsers) {
+    console.log(submittedUsers[0]);
+  };
 }
 
-
 module.exports = checkUsersValid;
+
+
+
+
+// use map on every index of the array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- program.js
+++ program.js
@@ -1,10 +1,13 @@
-// console.log("beep boop");
-var fs = require('fs');
-var through = require('through');
-var tr = through(write, end);
+// // console.log("beep boop");
+// var fs = require('fs'); 
+// var through = require('through');
+// var tr = through(write, end);
 
-tr.write();
-tr.end();
 
-function write (buf) { prepipe = process.stdin.pipe(tr).pipe(process.stdout) }
-function end () { this.queue(null) }
+
+// function write (buf) {  }
+
+// function end () { this.queue(null) }
+console.log(process.argv[2]);
+
+
